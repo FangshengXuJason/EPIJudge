f8a9965c6db3bf8ded160f46a1312e629e1cc023
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-03-07 17:46:24 -0800

    Update a_b_sqrt2.py

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -1,11 +1,25 @@
 from typing import List
-
 from test_framework import generic_test
 
+# added
+import bintrees
+import math
 
-def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
-    # TODO - you fill in here.
-    return []
+# copied from the solution
+class Number:
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+        self.val = a + b * math.sqrt(2)
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+# TODO: add my code
+# def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
+#     return []
 
 
 if __name__ == '__main__':
