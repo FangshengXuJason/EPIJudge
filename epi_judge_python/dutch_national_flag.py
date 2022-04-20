80fb9098101707e50dd9fc963f9e07482fb5948f
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-04-19 23:01:14 -0700

    Update dutch_national_flag.py

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -10,7 +10,38 @@ RED, WHITE, BLUE = range(3)
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
     # TODO - you fill in here.
-    return
+    # MORE INTUITIVE SOLUTION
+
+    # next_less, next_larger = 0, len(A) - 1
+    # p_val = A[pivot_index]
+
+    # # make "less than" partition
+    # for i in range(len(A)):
+    #     if A[i] < p_val: # swap
+    #         A[i], A[next_less] = A[next_less], A[i]
+    #         next_less += 1
+
+    # # make "larger than" partition
+    # for i in reversed(range(len(A))):
+    #     if A[i] > p_val:
+    #         A[i], A[next_larger] = A[next_larger], A[i]
+    #         next_larger -= 1
+
+    # DIFFERENT SOLUTION
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0, 0, len(A)
+    # keep iterating as long as there is an unclassified element
+    while equal < larger:
+        if A[equal] < pivot:
+            A[equal], A[smaller] = A[smaller], A[equal]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] is pivot:
+            equal += 1
+        else: # A[equal] > pivot
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+
+
 
 
 @enable_executor_hook
