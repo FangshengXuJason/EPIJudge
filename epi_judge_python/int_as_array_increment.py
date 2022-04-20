9ae1e906f2eec2bd1fbde96d9a3fefa0aeb8c148
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-04-20 00:12:43 -0700

    Update int_as_array_increment.py

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,11 +1,24 @@
 from typing import List
 
+from sqlalchemy import false, true
+
 from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    carry = true
+    for i in reversed(range(len(A))):
+        if A[i] is 9 and carry:
+            A[i] = 0
+        elif carry: # digit is not 9
+            A[i] += 1
+            return A
+    # need one more digit
+    if carry:
+        A[0] = 1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
